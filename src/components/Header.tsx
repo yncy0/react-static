15e98290d94f5ea3b1e47551c0277b0311a1c07d
yncy0 <yncyncy0@gmail.com>
2025-09-02 21:57:09 +0800

    finished: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import { SiReact } from "@icons-pack/react-simple-icons";
+
+export default function Header() {
+  type Items = {
+    id: string;
+    name: string;
+    link: string;
+  };
+
+  const items: Items[] = [
+    {
+      id: "1",
+      name: "Home",
+      link: "#",
+    },
+    {
+      id: "2",
+      name: "About",
+      link: "#",
+    },
+    {
+      id: "3",
+      name: "Docs",
+      link: "#",
+    },
+  ];
+
+  return (
+    <header className="flex flex-row w-screen justify-between items-center top-0 left-0 fixed py-10 px-64">
+      <SiReact color='default' size={48}/>
+      <nav>
+        <ul className="flex flex-row gap-5">
+          {items.map((item) => (
+            <li className="text-black poppins-regular">{item.name}</li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.tsx'
+import Header from './components/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Header />
     <App />
   </StrictMode>,
 )
